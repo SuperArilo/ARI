294eb50433465a2f2ced74d86f75690bdeae3eb5
SuperArilo <728152568@qq.com>
2025-12-09 16:01:51 +0800

    update

--- src/main/java/com/tty/Loader.java
+++ src/main/java/com/tty/Loader.java
@@ -15,6 +15,7 @@ public class Loader implements PluginLoader {
         MavenLibraryResolver resolver = new MavenLibraryResolver();
         resolver.addDependency(new Dependency(new DefaultArtifact("com.zaxxer", "HikariCP", "jar", "5.1.0"), "provided"));
         resolver.addDependency(new Dependency(new DefaultArtifact("org.sql2o", "sql2o", "jar", "1.9.1"), "provided"));
+        resolver.addDependency(new Dependency(new DefaultArtifact("mysql", "mysql-connector-java", "jar", "8.1.1"), "provided"));
 
         resolver.addRepository(new RemoteRepository.Builder("paper", "default", "https://repo.papermc.io/repository/maven-public/").build());
         resolver.addRepository(new RemoteRepository.Builder("central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
