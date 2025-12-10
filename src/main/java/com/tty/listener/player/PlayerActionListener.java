bb0126ee2f353b8219fa1aa96eb33fcd448eb077
SuperArilo <728152568@qq.com>
2025-12-10 11:10:09 +0800

    修复可能会触发两次的问题

--- src/main/java/com/tty/listener/player/PlayerActionListener.java
+++ src/main/java/com/tty/listener/player/PlayerActionListener.java
@@ -14,40 +14,44 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.*;
 import org.bukkit.Material;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class PlayerActionListener implements Listener {
 
     @EventHandler
-    public void onPlayInteract(PlayerInteractEvent event) {
-        //未开启
+    public void onPlayerInteract(PlayerInteractEvent event) {
         if (!this.isNotEnableSit()) return;
+
         Player player = event.getPlayer();
-        //动作不匹配
+        // 只处理主手
+        if (event.getHand() != EquipmentSlot.HAND) return;
+        // 动作不匹配或旁观模式
         if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || player.getGameMode().equals(GameMode.SPECTATOR)) return;
-        //右手不为空
+        // 手持空
         if (event.getItem() != null || !player.getInventory().getItemInMainHand().getType().isAir()) return;
-        //判断被点击的方块是否存在
+        // 点击的方块不存在
         Block clickedBlock = event.getClickedBlock();
         if (clickedBlock == null) return;
-        //判断发起动作的玩家是否有骑乘实体
-
+        // 玩家已经骑乘实体
         if (player.getVehicle() != null) return;
-
         Ari.instance.stateMachineManager
                 .get(PlayerSitActionStateService.class)
                 .addState(new PlayerSitActionState(player, clickedBlock));
     }
+
     //玩家相互骑乘
     @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-        //未开启
+        // 未开启
         if (!this.isNotEnablePlayerSitPlayer()) return;
-        //发起骑乘的玩家
+        // 只处理主手
+        if (event.getHand() != EquipmentSlot.HAND) return;
         Player player = event.getPlayer();
-        //右手必须为空才能骑乘
-        if (!player.getInventory().getItemInMainHand().getType().equals(Material.AIR) || player.getGameMode().equals(GameMode.SPECTATOR)) return;
-        //被点击的实体必须属于玩家
-        if(!(event.getRightClicked() instanceof Player clickedPlayer)) return;
+        // 玩家必须空手且不是旁观模式
+        if (!player.getInventory().getItemInMainHand().getType().equals(Material.AIR)
+                || player.getGameMode().equals(GameMode.SPECTATOR)) return;
+        // 被点击的实体必须是玩家
+        if (!(event.getRightClicked() instanceof Player clickedPlayer)) return;
 
         Ari.instance.stateMachineManager
                 .get(PlayerRideActionStateService.class)
