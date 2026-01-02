dffed4f7e589e37e9c7ec0ce50a3617531f5773c
SuperArilo <superarilowh@gmail.com>
2026-01-02 20:54:25 +0800

    更新死亡信息追踪

--- src/main/java/com/tty/tool/PlayerDeathInfoCollector.java
+++ src/main/java/com/tty/tool/PlayerDeathInfoCollector.java
@@ -3,12 +3,15 @@ package com.tty.tool;
 import com.google.common.reflect.TypeToken;
 import com.tty.Ari;
 import com.tty.enumType.FilePath;
+import com.tty.lib.Log;
 import com.tty.lib.tool.PublicFunctionUtils;
 import org.bukkit.Location;
+import org.bukkit.damage.DamageSource;
 import org.bukkit.entity.*;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.Type;
@@ -46,30 +49,41 @@ public class PlayerDeathInfoCollector {
         public String getRandomOfList(String keyPath) {
             String killerName = this.killer == null ? null : this.killer.getType().name().toLowerCase();
             Type type = new TypeToken<List<String>>() {}.getType();
-            List<String> pool = new ArrayList<>();
 
+            List<String> pool = new ArrayList<>();
+            List<String> resolvedKeys = new ArrayList<>();
             List<String> publicList = Ari.C_INSTANCE.getValue(keyPath + ".public", FilePath.DEATH_MESSAGE, type, null);
 
             if (publicList != null && !publicList.isEmpty()) {
                 pool.addAll(publicList);
+                resolvedKeys.add(keyPath + ".public");
+
                 if (killerName != null) {
                     List<String> killerList = Ari.C_INSTANCE.getValue(keyPath + "." + killerName, FilePath.DEATH_MESSAGE, type, null);
-                    if (killerList != null && !killerList.isEmpty()) pool.addAll(killerList);
+                    if (killerList != null && !killerList.isEmpty()) {
+                        pool.addAll(killerList);
+                        resolvedKeys.add(keyPath + "." + killerName);
+                    }
                 }
             } else {
                 if (killerName != null) {
-                    List<String> killerList = Ari.C_INSTANCE.getValue(keyPath + "." + killerName, FilePath.DEATH_MESSAGE, type,null);
-                    if (killerList != null && !killerList.isEmpty()) pool.addAll(killerList);
-                    else {
+                    List<String> killerList = Ari.C_INSTANCE.getValue(keyPath + "." + killerName, FilePath.DEATH_MESSAGE, type, null);
+                    if (killerList != null && !killerList.isEmpty()) {
+                        pool.addAll(killerList);
+                        resolvedKeys.add(keyPath + "." + killerName);
+                    } else {
                         List<String> fallbackList = Ari.C_INSTANCE.getValue(keyPath, FilePath.DEATH_MESSAGE, type, null);
-                        if (fallbackList != null && !fallbackList.isEmpty()) pool.addAll(fallbackList);
+                        if (fallbackList != null && !fallbackList.isEmpty()) {
+                            pool.addAll(fallbackList);
+                            resolvedKeys.add(keyPath);
+                        }
                     }
                 }
             }
+            Log.debug("full key path: %s", String.join(", ", resolvedKeys));
             return pool.isEmpty() ? "" :
                     pool.get(PublicFunctionUtils.randomGenerator(0, pool.size() - 1));
         }
-
     }
 
     public DeathInfo collect(PlayerDeathEvent event) {
@@ -92,16 +106,26 @@ public class PlayerDeathInfoCollector {
             if (mainKiller != null) {
                 info.killer = mainKiller;
                 info.isEntityCause = true;
-
-                double escapeDistance = info.isProjectile ? 1.5 : 20.0;
-                Location lastLoc = last.location();
+                double escapeDistance = !info.isProjectile ? 1.5 : 20.0;
+                Location lastLoc = records.getFirst().location();
                 Location deathLoc = info.victim.getLocation();
                 if (lastLoc != null && lastLoc.getWorld() == deathLoc.getWorld()) {
+                    double sqDist = lastLoc.distanceSquared(deathLoc);
+                    Log.debug("Distance squared: %s, Threshold: %s", sqDist, (escapeDistance * escapeDistance));
                     info.isEscapeAttempt = lastLoc.distanceSquared(deathLoc) > escapeDistance * escapeDistance;
                 }
 
                 info.weapon = last.weapon();
             }
+            return info;
+        }
+        DamageSource damageSource = event.getDamageSource();
+
+        info.killer = damageSource.getCausingEntity();
+        if (info.killer instanceof LivingEntity e) {
+            EntityEquipment equipment = e.getEquipment();
+            info.weapon = equipment == null ? null:equipment.getItemInMainHand();
+            return info;
         }
 
         if (info.deathCause == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) {
