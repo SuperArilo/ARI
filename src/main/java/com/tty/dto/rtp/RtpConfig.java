3b98619063735effd0aebbbbb0f1fb91034ccb64
SuperArilo <superarilowh@gmail.com>
2026-04-15 19:28:11 +0800

    update

--- src/main/java/com/tty/dto/rtp/RtpConfig.java
+++ src/main/java/com/tty/dto/rtp/RtpConfig.java
@@ -1,14 +1,26 @@
 package com.tty.dto.rtp;
 
 import com.google.gson.annotations.Expose;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
 public class RtpConfig {
+
     @Expose
+    @Getter
     private boolean enable = true;
     @Expose
+    @Getter
     private Integer min = 300;
     @Expose
+    @Getter
     private Integer max = 1500;
+
+    public RtpConfig() {}
+
+    public RtpConfig(boolean enable, int min, int max) {
+        this.enable = enable;
+        this.min = min;
+        this.max = max;
+    }
+
 }
