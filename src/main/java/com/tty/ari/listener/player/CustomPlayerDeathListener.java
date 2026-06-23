8867928a7f30b6535bfb6702986b83fc7b2ba20f
SuperArilo <superarilowh@gmail.com>
2026-06-23 19:32:10 +0800

    修复可能的线程调度问题导致自定义死亡信息错误的问题

--- src/main/java/com/tty/ari/listener/player/CustomPlayerDeathListener.java
+++ src/main/java/com/tty/ari/listener/player/CustomPlayerDeathListener.java
@@ -8,7 +8,9 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Explosive;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByBlockEvent;
@@ -17,6 +19,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class CustomPlayerDeathListener implements Listener {
 
@@ -33,100 +36,112 @@ public class CustomPlayerDeathListener implements Listener {
 
         CompletableFuture<Component> killerFuture = tool.setEntityHoverText(info.killer)
             .thenComposeAsync(killerComp -> {
-                if (info.killer.equals(info.victim) && info.killer instanceof Player) {
-                    return CompletableFuture.completedFuture(
-                            tool.text(Ari.DATA_SERVICE.getValue("base.on-player.self"))
-                    );
-                }
+                if (info.killer == null) return CompletableFuture.completedFuture(Component.empty());
+
+                if (info.killer.equals(info.victim) && info.killer instanceof Player) return CompletableFuture.completedFuture(tool.text(Ari.DATA_SERVICE.getValue("base.on-player.self")));
+
                 return CompletableFuture.completedFuture(killerComp);
-            }, Ari.instance.getExecutorSync());
+            }, Ari.instance.getExecutorAsync()).exceptionally(e -> {
+                Ari.instance.getLog().error(e);
+                return null;
+            }).orTimeout(1, TimeUnit.SECONDS);
 
-        CompletableFuture<Component> victimFuture = tool.setEntityHoverText(info.victim);
-        CompletableFuture<Component> weaponFuture = tool.setHoverItemText(info.killer, info.weapon);
+        CompletableFuture<Component> victimFuture = tool.setEntityHoverText(info.victim).orTimeout(1, TimeUnit.SECONDS);
+        CompletableFuture<Component> weaponFuture = tool.setHoverItemText(info.weapon).orTimeout(1, TimeUnit.SECONDS);
+        CompletableFuture<String> messageFuture = this.getDeathMessage(info, event).orTimeout(1, TimeUnit.SECONDS);
 
-        CompletableFuture.allOf(killerFuture, victimFuture, weaponFuture)
-            .thenRunAsync(() -> {
+        CompletableFuture.allOf(killerFuture, victimFuture, weaponFuture, messageFuture).thenRunAsync(() -> {
                 Component killComp = killerFuture.join();
                 Component victimComp = victimFuture.join();
                 Component weaponComp = weaponFuture.join();
+                String message = messageFuture.join();
 
-                Component deathMsg = tool.text(this.getDeathMessage(info, event), Map.of(
+                Component deathMsg = tool.text(message, Map.of(
                         LangPlayerDeath.VICTIM_UNRESOLVED.getType(), victimComp,
                         LangPlayerDeath.KILLER_UNRESOLVED.getType(), killComp,
-                        LangPlayerDeath.KILLER_ITEM_UNRESOLVED.getType(), weaponComp
-                ));
+                        LangPlayerDeath.KILLER_ITEM_UNRESOLVED.getType(), weaponComp));
                 Bukkit.getServer().broadcast(deathMsg);
-            }, Ari.instance.getExecutorSync());
+
+        }, Ari.instance.getExecutorSync()).exceptionally(e -> {
+            Ari.instance.getLog().error(e);
+            return null;
+        });
     }
 
-    private String getDeathMessage(PlayerDeathInfoCollector.DeathInfo info, PlayerDeathEvent event) {
+    private CompletableFuture<String> getDeathMessage(PlayerDeathInfoCollector.DeathInfo info, PlayerDeathEvent event) {
+
+        CompletableFuture<String> future = new CompletableFuture<>();
+        Player entity = event.getEntity();
 
         String baseKey = "custom-death.";
         switch (info.deathCause) {
-            case ENTITY_EXPLOSION, BLOCK_EXPLOSION -> {
-                return info.getRandomOfList(baseKey + (info.killer instanceof Explosive ? "player.explosion" : "mob.explosion"), info.isDestine);
-            }
-            case ENTITY_ATTACK, ENTITY_SWEEP_ATTACK, PROJECTILE, POISON -> {
-                String weaponKey = this.getWeaponKey(info);
-                String targetKey = info.killer instanceof Player ? "player" : "mob";
-                String message = info.getRandomOfList(baseKey + targetKey + "." + weaponKey, info.isDestine);
+            case ENTITY_EXPLOSION, BLOCK_EXPLOSION -> future.complete(info.getRandomOfList(baseKey + (info.killer instanceof Explosive ? "player.explosion" : "mob.explosion"), info.isDestine));
+            case ENTITY_ATTACK, ENTITY_SWEEP_ATTACK, PROJECTILE, POISON ->
+                    Ari.instance.getScheduler().runAtEntity(Ari.instance, entity, i -> {
+                        String weaponKey = this.getWeaponKey(info);
+                        String targetKey = info.killer instanceof Player ? "player" : "mob";
+                        String message = info.getRandomOfList(baseKey + targetKey + "." + weaponKey, info.isDestine);
 
-                if (info.isEscapeAttempt) {
-                    message += info.getRandomOfList(baseKey + "running-away");
-                }
-                return message;
-            }
-            case CONTACT, LAVA, HOT_FLOOR -> {
-                if (event.getEntity().getLastDamageCause() instanceof EntityDamageByBlockEvent blockEvent) {
-                    Block block = blockEvent.getDamager();
-                    if (block == null) {
-                        Ari.instance.getLog().error("can not find contact block");
-                        return null;
-                    }
-                    return info.getRandomOfList(baseKey + "block." + block.getType().name().toLowerCase(), info.isDestine);
-                }
-                return null;
-            }
-            case FALLING_BLOCK -> {
-                Material material = null;
-                String key = "";
-                if (event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent entityEvent) {
-                    if (entityEvent.getDamager() instanceof FallingBlock fallingBlock) {
-                        material = fallingBlock.getBlockData().getMaterial();
-                        key = switch (material) {
-                            case ANVIL, CHIPPED_ANVIL, DAMAGED_ANVIL -> "anvil";
-                            default -> material.name().toLowerCase();
-                        };
-                    } else {
-                        Ari.instance.getLog().error("can not find falling block");
-                        return null;
-                    }
-                }
+                        if (info.isEscapeAttempt) {
+                            message += info.getRandomOfList(baseKey + "running-away");
+                        }
+                        future.complete(message);
+                    }, null);
+            case CONTACT, LAVA, HOT_FLOOR ->
+                    Ari.instance.getScheduler().runAtEntity(Ari.instance, entity, i -> {
+                        if (event.getEntity().getLastDamageCause() instanceof EntityDamageByBlockEvent blockEvent) {
+                            Block block = blockEvent.getDamager();
+                            if (block == null) {
+                                Ari.instance.getLog().error("can not find contact block");
+                                future.complete(null);
+                            } else {
+                                future.complete(info.getRandomOfList(baseKey + "block." + block.getType().name().toLowerCase(), info.isDestine));
+                            }
+                        }
+                    }, null);
+            case FALLING_BLOCK ->
+                    Ari.instance.getScheduler().runAtEntity(Ari.instance, entity, i -> {
+                        Material material = null;
+                        String key = "";
+                        if (event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent entityEvent) {
+                            if (entityEvent.getDamager() instanceof FallingBlock fallingBlock) {
+                                material = fallingBlock.getBlockData().getMaterial();
+                                key = switch (material) {
+                                    case ANVIL, CHIPPED_ANVIL, DAMAGED_ANVIL -> "anvil";
+                                    default -> material.name().toLowerCase();
+                                };
+                            } else {
+                                Ari.instance.getLog().error("can not find falling block");
+                                future.complete(null);
+                                return;
+                            }
+                        }
 
-                if (material == null) {
-                    Ari.instance.getLog().error("custom-death: can not find material data");
-                    return null;
-                }
-                return info.getRandomOfList(baseKey + "falling-blocks." + key, info.isDestine);
-            }
-            case FALL -> { return info.getRandomOfList(baseKey + "player.fall", info.isDestine); }
-            case FIRE, FIRE_TICK, CAMPFIRE -> { return info.getRandomOfList(baseKey + "player.fire", info.isDestine); }
-            case LIGHTNING -> { return info.getRandomOfList(baseKey + "player.lightning", info.isDestine); }
-            case SUFFOCATION -> { return info.getRandomOfList(baseKey + "player.suffocation", info.isDestine); }
-            case DROWNING -> { return info.getRandomOfList(baseKey + "player.drowning", info.isDestine); }
-            case FREEZE -> { return info.getRandomOfList(baseKey + "player.freeze", info.isDestine); }
-            case SUICIDE -> { return info.getRandomOfList(baseKey + "player.suicide", info.isDestine); }
-            case VOID -> { return info.getRandomOfList(baseKey + "player.void", info.isDestine); }
-            case WITHER -> { return info.getRandomOfList(baseKey + "player.wither", info.isDestine); }
-            case FLY_INTO_WALL -> { return info.getRandomOfList(baseKey + "player.fly_into_wall", info.isDestine); }
-            case KILL -> { return info.getRandomOfList(baseKey + "player.kill", info.isDestine); }
-            case MAGIC -> { return info.getRandomOfList(baseKey + "player.magic", info.isDestine); }
-            case STARVATION -> { return info.getRandomOfList(baseKey + "player.starvation", info.isDestine); }
-            case SONIC_BOOM -> { return info.getRandomOfList(baseKey + "player.sonic_boom", info.isDestine); }
-            case THORNS -> { return info.getRandomOfList(baseKey + "player.thorns", info.isDestine); }
-
-            default -> { return null; }
+                        if (material == null) {
+                            Ari.instance.getLog().error("custom-death: can not find material data");
+                            future.complete(null);
+                            return;
+                        }
+                        future.complete(info.getRandomOfList(baseKey + "falling-blocks." + key, info.isDestine));
+                    }, null);
+            case FALL -> future.complete(info.getRandomOfList(baseKey + "player.fall", info.isDestine));
+            case FIRE, FIRE_TICK, CAMPFIRE -> future.complete(info.getRandomOfList(baseKey + "player.fire", info.isDestine));
+            case LIGHTNING -> future.complete(info.getRandomOfList(baseKey + "player.lightning", info.isDestine));
+            case SUFFOCATION -> future.complete(info.getRandomOfList(baseKey + "player.suffocation", info.isDestine));
+            case DROWNING -> future.complete(info.getRandomOfList(baseKey + "player.drowning", info.isDestine));
+            case FREEZE -> future.complete(info.getRandomOfList(baseKey + "player.freeze", info.isDestine));
+            case SUICIDE -> future.complete(info.getRandomOfList(baseKey + "player.suicide", info.isDestine));
+            case VOID -> future.complete(info.getRandomOfList(baseKey + "player.void", info.isDestine));
+            case WITHER -> future.complete(info.getRandomOfList(baseKey + "player.wither", info.isDestine));
+            case FLY_INTO_WALL -> future.complete(info.getRandomOfList(baseKey + "player.fly_into_wall", info.isDestine));
+            case KILL -> future.complete(info.getRandomOfList(baseKey + "player.kill", info.isDestine));
+            case MAGIC -> future.complete(info.getRandomOfList(baseKey + "player.magic", info.isDestine));
+            case STARVATION -> future.complete(info.getRandomOfList(baseKey + "player.starvation", info.isDestine));
+            case SONIC_BOOM -> future.complete(info.getRandomOfList(baseKey + "player.sonic_boom", info.isDestine));
+            case THORNS -> future.complete(info.getRandomOfList(baseKey + "player.thorns", info.isDestine));
+            default -> future.complete(null);
         }
+        return future;
     }
 
     private String getWeaponKey(PlayerDeathInfoCollector.DeathInfo info) {
