4a408eb067dfb26f1ab9fd7f4a54778c29d03d0e
SuperArilo <728152568@qq.com>
2026-07-23 09:06:53 +0800

    修复服务器维护模式开启后关闭也会强制踢出玩家

--- src/main/java/com/tty/ari/commands/maintenance.java
+++ src/main/java/com/tty/ari/commands/maintenance.java
@@ -4,6 +4,7 @@ import com.tty.api.ComponentTool;
 import com.tty.api.annotations.command.CommandMeta;
 import com.tty.api.annotations.command.LiteralCommand;
 import com.tty.api.command.SuperHandsomeCommand;
+import com.tty.api.scheduler.RunTask;
 import com.tty.ari.Ari;
 import com.tty.ari.command.LiteralArgumentCommand;
 import com.tty.ari.dto.state.player.MaintenanceBossBarState;
@@ -15,10 +16,14 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @CommandMeta(displayName = "maintenance", permission = "ari.command.maintenance", tokenLength = 1, allowConsole = true)
 @LiteralCommand(directExecute = true)
 public class maintenance extends LiteralArgumentCommand {
+
+    private final Map<Player, RunTask> playerTaskMap = new ConcurrentHashMap<>();
 
     @Override
     public List<SuperHandsomeCommand> thenCommands() {
@@ -30,31 +35,48 @@ public class maintenance extends LiteralArgumentCommand {
         MaintenanceBossBarService service = Ari.instance.getStatusManager().get(MaintenanceBossBarService.class);
         service.setMaintenance(!service.isMaintenance());
 
-        Component component = ConfigUtils.tAfter("server.maintenance." + (service.isMaintenance() ? "on-enable" : "on-disable"));
-        boolean isMaintenance = service.isMaintenance();
+        Component enable = ConfigUtils.tAfter("server.maintenance.on-enable");
+        Component disable = ConfigUtils.tAfter("server.maintenance.on-disable");
 
+        boolean isMaintenance = service.isMaintenance();
         for (Player player : Bukkit.getServer().getOnlinePlayers()) {
             if (player.isOp()) {
-                player.sendMessage(component);
+                player.sendMessage(isMaintenance ? enable : disable);
                 if (isMaintenance) {
                     service.addState(new MaintenanceBossBarState(player));
                 } else {
                     service.stopStateByOwner(player);
                 }
-                continue;
-            }
-            if (isMaintenance) {
-                ConfigUtils.t("server.maintenance.to-player", player).thenAccept(player::sendMessage);
-                Ari.instance.getScheduler().runAtEntityLater(player, i -> {
-                    if (!player.isOnline() || !service.isMaintenance() || player.isOp()) return;
-                    player.kick(ComponentTool.text(Ari.DATA_SERVICE.getValue("base.on-player.data-changed")));
-                },
-                null, this.getMaintenanceKickDelay() * 20L);
+            } else {
+                if (isMaintenance) {
+                    ConfigUtils.t("server.maintenance.to-player", player).thenAccept(player::sendMessage);
+                    RunTask old = this.playerTaskMap.remove(player);
+                    if (old != null) old.cancel();
+
+                    RunTask runTask = Ari.instance.getScheduler().runAtEntityLater(player, i -> {
+                        try {
+                            if (!player.isOnline() || !service.isMaintenance() || player.isOp()) return;
+                            player.kick(ComponentTool.text(Ari.DATA_SERVICE.getValue("base.on-player.data-changed")));
+                        } finally {
+                            this.playerTaskMap.remove(player);
+                        }
+                    }, null, getMaintenanceKickDelay() * 20L);
+                    this.playerTaskMap.put(player, runTask);
+                } else {
+                    player.sendMessage(disable);
+                }
             }
         }
 
+        if (!isMaintenance) {
+            for (RunTask task : this.playerTaskMap.values()) {
+                if (task != null) task.cancel();
+            }
+            this.playerTaskMap.clear();
+        }
+
         if (!(sender instanceof Player)) {
-            sender.sendMessage(component);
+            sender.sendMessage(isMaintenance ? enable : disable);
         }
     }
 
