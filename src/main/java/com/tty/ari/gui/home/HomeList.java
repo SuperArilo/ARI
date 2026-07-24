b14dba43d21e3c4258c76383a7a26b0d79270c0f
SuperArilo <728152568@qq.com>
2026-07-24 10:39:43 +0800

    update

--- src/main/java/com/tty/ari/gui/home/HomeList.java
+++ src/main/java/com/tty/ari/gui/home/HomeList.java
@@ -26,7 +26,6 @@ import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
